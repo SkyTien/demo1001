f31701d613ccc8c730c5b0d120b31561652b8be2
Will <doggy.huang@gmail.com>
2016-10-01 15:44:22 +0800

    示範不同的 Hook Methods

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent implements OnInit, OnChanges {
 
   @Input() item: any;
 
@@ -14,6 +14,12 @@ export class ArticleComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+  }
+
+  ngDoCheck() {
   }
 
   doDelete(item) {
